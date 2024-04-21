e64d06c7898f7cfa46f45de7e7f64c8046751a20
apppeanuts <ygmasa2815@gmail.com>
2024-04-21 18:15:51 +0900

    あきらめ

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const resultnum = document.getElementById('resultnum');
 const one = document.getElementById('one');
 const nums = document.querySelectorAll('#num');
+const ac = document.getElementById('ac');
+const plus = document.getElementById('plus');
 
 const arr = [];
+
 for (const num of nums) {
     num.addEventListener('click', () => {
-        arr.push(num.textContent);
+        arr.push(parseInt(num.textContent));
     });
 }
+
+ac.addEventListener('click', () => {
+    arr = [];
+});
+
+let total = 0;
+function sum(iterable) {
+    for (let num of iterable) {
+        total += num;
+    }
+    return total;
+}
+
+plus.addEventListener('click', () => {
+    for (const num of nums) {
+        num.addEventListener('click', () => {
+            sum(arr);
+        });
+    }
+});

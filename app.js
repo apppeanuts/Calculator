bd495cff27480cd98fddb283ae091715a00f6219
apppeanuts <ygmasa2815@gmail.com>
2024-04-21 17:35:14 +0900

    配列代入完了2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const resultnum = document.getElementById('resultnum');
 const one = document.getElementById('one');
-const num = document.querySelectorAll('#num');
+const nums = document.querySelectorAll('#num');
 
 const arr = [];
-for (const button of num) {
-    button.addEventListener('click', () => {
-        arr.push(button.textContent);
+for (const num of nums) {
+    num.addEventListener('click', () => {
+        arr.push(num.textContent);
     });
 }

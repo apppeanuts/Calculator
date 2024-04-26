a94873b94994901e0890810528754b67ae55396c
apppeanuts <ygmasa2815@gmail.com>
2024-04-27 01:30:42 +0900

    eval後、自力で完成

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# Calculator
-Calculator with general functions.
+## Calculator
+Calculator with minimum functions.
 
-# Framework
-Use [Bootstrap](https://getbootstrap.jp/docs/5.3/getting-started/introduction/) as HTML / CSS framework.
+## Framework
+Use [Bootstrap](https://getbootstrap.jp/docs/5.3/getting-started/introduction/) as `HTML` / `CSS` framework.
 
-# Developed by
+## Developed by
 - [Masa](https://github.com/masataisyou)
+    - Mainly responsible for `HTML`, `CSS`.
 - [Yg](https://github.com/clumsyg)
+    - Mainly responsible for `JavaScript`.
+
+## Attention
+[Rounding errors](https://e-words.jp/w/%E4%B8%B8%E3%82%81%E8%AA%A4%E5%B7%AE.html) may result in incorrect calculation results. e.g. 9.7 + 2.1.
--- app.js
+++ app.js
@@ -1,33 +1,84 @@
 const resultnum = document.getElementById('resultnum');
-const one = document.getElementById('one');
 const nums = document.querySelectorAll('#num');
+const addition = document.getElementById('addition');
+const subtraction = document.getElementById('subtraction');
+const multiplication = document.getElementById('multiplication');
+const division = document.getElementById('division');
+const percent = document.getElementById('percent');
+const plusminus = document.getElementById('plusminus');
+const period = document.getElementById('period');
+const c = document.getElementById('c');
 const ac = document.getElementById('ac');
-const plus = document.getElementById('plus');
+const equal = document.getElementById('equal');
 
-const arr = [];
+let numCount = 0;  // 数字が押されるたびに1増える
+let symbolCount = 0;  // 四則演算記号が押されるたびに1増える
 
-for (const num of nums) {
+for (let num of nums) {
     num.addEventListener('click', () => {
-        arr.push(parseInt(num.textContent));
+        numCount++;
+        // numCount !== 1の必要性: numCount === 1の時は初めて数字を押したときなので、初めての時に+=してしまうと、最初の0に対して、押された数字xが+=されて0xとなってしまうから。
+        // symbolCount === 1の必要性: 初めて四則演算記号が押されたとき、という意味。その記号に押された数字を+=する。これによって、デフォルトの0から数字を押す前にいきなり記号を押したとしても、0-〇とか0+〇とかのように0からの計算が可能となる。
+        if (numCount !== 1 || symbolCount === 1) {
+            resultnum.textContent += num.textContent;
+        } else {
+            resultnum.textContent = num.textContent;
+        }
     });
 }
 
-ac.addEventListener('click', () => {
-    arr = [];
+addition.addEventListener('click', () => {
+    symbolCount++;
+    resultnum.textContent += '+';
 });
 
-let total = 0;
-function sum(iterable) {
-    for (let num of iterable) {
-        total += num;
-    }
-    return total;
-}
+subtraction.addEventListener('click', () => {
+    symbolCount++;
+    resultnum.textContent += '-';
+});
 
-plus.addEventListener('click', () => {
-    for (const num of nums) {
-        num.addEventListener('click', () => {
-            sum(arr);
-        });
-    }
+multiplication.addEventListener('click', () => {
+    symbolCount++;
+    resultnum.textContent += '*';
+});
+
+division.addEventListener('click', () => {
+    symbolCount++;
+    resultnum.textContent += '/';
+});
+
+// 丸め誤差の影響により、9.7 + 2.1などは計算結果が正解とずれる
+period.addEventListener('click', () => {
+    symbolCount++;
+    resultnum.textContent += '.';
+});
+
+percent.addEventListener('click', () => {
+    resultnum.textContent /= 100;
+});
+
+// numのaddEventListener内に書く必要ありそうと思ってたが、ここでokなよう
+plusminus.addEventListener('click', () => {
+    let lastNum = resultnum.textContent[resultnum.textContent.length - 1];
+    // slice(0, -1)なので、最初~最後から2番目となる。最後は含まないことが重要
+    resultnum.textContent = resultnum.textContent.slice(0, -1) + (-lastNum);
+});
+
+equal.addEventListener('click', () => {
+    resultnum.textContent = eval(resultnum.textContent);
+});
+
+c.addEventListener('click', () => {
+    symbolCount++;
+    // 配列と違い文字列は個々の要素を変更することができないため、以下では変化なし
+    // resultnum.textContent[resultnum.textContent.length - 1] = '';
+    resultnum.textContent = resultnum.textContent.slice(
+        0, resultnum.textContent.length - 1
+    );
+});
+
+ac.addEventListener('click', () => {
+    resultnum.textContent = 0;
+    numCount = 0;
+    symbolCount = 0;
 });
